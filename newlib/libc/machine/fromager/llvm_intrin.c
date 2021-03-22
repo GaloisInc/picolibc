7091267b2102bbb553e005466871f637a5751298
Stuart Pernsteiner <spernsteiner@galois.com>
2021-03-22 10:39:10 -0700

    add llvm.bswap.i32 and llvm.ctpop.i32 implementations

--- newlib/libc/machine/fromager/llvm_intrin.c
+++ newlib/libc/machine/fromager/llvm_intrin.c
@@ -11,3 +11,20 @@ void __llvm__memset__p0i8__i64(uint8_t *dest, uint8_t val, uint64_t len) {
         dest[i] = val;
     }
 }
+
+uint32_t __llvm__bswap__i32(uint32_t x) {
+    uint8_t a = x >> 0;
+    uint8_t b = x >> 8;
+    uint8_t c = x >> 16;
+    uint8_t d = x >> 24;
+    return d | (c << 8) | (b << 16) | (a << 24);
+}
+
+uint32_t __llvm__ctpop__i32(uint32_t x) {
+    x = (x & 0x55555555) + ((x & 0xaaaaaaaa) >> 1);
+    x = (x & 0x33333333) + ((x & 0xcccccccc) >> 2);
+    x = (x & 0x0f0f0f0f) + ((x & 0xf0f0f0f0) >> 4);
+    x = (x & 0x00ff00ff) + ((x & 0xff00ff00) >> 8);
+    x = (x & 0x0000ffff) + ((x & 0xffff0000) >> 16);
+    return x;
+}
