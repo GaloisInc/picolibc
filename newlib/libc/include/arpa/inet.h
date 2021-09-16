876e42efb53298ff826bdd84b19b9034c66c6cf3
James Parker <james@galois.com>
2021-09-16 17:28:46 +0000

    socket headers

---
+++ newlib/libc/include/arpa/inet.h
@@ -0,0 +1,12 @@
+
+#ifndef _ARPA_INET_H_
+#define _ARPA_INET_H_ 1
+
+/* Type for length arguments in socket calls.  */
+#ifndef __socklen_t_defined
+typedef __socklen_t socklen_t;
+# define __socklen_t_defined
+#endif
+
+
+#endif
