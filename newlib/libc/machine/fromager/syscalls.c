82ccd4529cddf71124114ee9993f2026cb894e9e
Stuart Pernsteiner <spernsteiner@galois.com>
2021-10-12 16:47:09 +0000

    add stubs for fstat, poll, and setsockopt

--- newlib/libc/machine/fromager/syscalls.c
+++ newlib/libc/machine/fromager/syscalls.c
@@ -6,6 +6,8 @@
 #include <sys/time.h>
 #include <sys/unistd.h>
 #include <unistd.h>
+#include <poll.h>
+#include <sys/stat.h>
 #include "fromager.h"
 #include "noni.h"
 
@@ -61,6 +63,11 @@ int socket(int domain, int type, int protocol) {
     return 0;
 }
 
+int setsockopt(int sockfd, int level, int optname, const void *optval, socklen_t optlen) {
+    // __cc_valid_if(0, "unimplemented setsockopt");
+    return 0;
+}
+
 int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
     // __cc_valid_if(0, "unimplemented bind");
     return 0;
@@ -99,12 +106,20 @@ int shutdown(int sockfd, int how) {
     __cc_valid_if(0, "unimplemented shutdown");
 }
 
+int fstat(int fd, struct stat* st) {
+    __cc_valid_if(0, "unimplemented fstat");
+}
+
 off_t lseek(int fd, off_t offset, int whence) {
     __cc_valid_if(0, "unimplemented lseek");
 }
 
 int ioctl(int fd, unsigned long request, ...) {
     __cc_valid_if(0, "unimplemented ioctl");
+}
+
+int poll(struct pollfd *fds, nfds_t nfds, int timeout) {
+    __cc_valid_if(0, "unimplemented poll");
 }
 
 extern unsigned char read_buf_0[32];
